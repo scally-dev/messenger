25d7e725a0e3b5c5e8d183c0a6bf691a2ace8118
formosus <scally.dev@gmail.com>
2023-04-05 21:50:38 +0300

    Тулбар и боковое меню

--- app/src/main/java/com/example/mymessenger/MainActivity.kt
+++ app/src/main/java/com/example/mymessenger/MainActivity.kt
@@ -3,13 +3,65 @@ package com.example.mymessenger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mymessenger.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder().withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                .withName("Artyom")
+                .withEmail("scally.dev@gmail.com")
+            ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
     }
 }
